e3bfe52083614d4d3f3c46e7db59bc2eb6af8a25
xuwei-k <6b656e6a69@gmail.com>
2014-11-09 04:17:03 +0900

    Stream MonadPlus, Foldable

--- free/src/main/java/free/Stream.java
+++ free/src/main/java/free/Stream.java
@@ -23,6 +23,22 @@ public abstract class Stream<A> implements _1<Stream.z, A> {
 
   public abstract boolean isEmpty();
 
+  public final <B> Stream<B> flatMap(final F1<A, Stream<B>> f){
+    return foldRight(() -> empty(), (h, t) -> f.apply(h).append(t));
+  }
+
+  public final Stream<A> append(final F0<Stream<A>> stream){
+    return foldRight(stream, (h, t) -> new Cons<>(h, Lazy.of(t)));
+  }
+
+  public final <B> B foldRight(final F0<B> z, final F2<A, F0<B>, B> f){
+    if(isEmpty()){
+      return z.apply();
+    }else{
+      return f.apply(unsafeHead(), () -> unsafeTail().foldRight(z, f));
+    }
+  }
+
   public final boolean nonEmpty(){
     return !isEmpty();
   }
@@ -99,6 +115,10 @@ public abstract class Stream<A> implements _1<Stream.z, A> {
     return (Stream<B>)empty;
   }
 
+  public static <B> Stream<B> fromList(final List<B> list){
+    return list.foldRight(Stream.<B>empty(), (h, t) -> new Cons<>(h, Lazy.value(t)));
+  }
+
   private final static class Empty<A> extends Stream<A>{
 
     @Override
@@ -124,6 +144,53 @@ public abstract class Stream<A> implements _1<Stream.z, A> {
     @Override
     public boolean isEmpty() {
       return true;
+    }
+  }
+
+  public static <B> Stream<B> narrow(final _1<z, B> a){
+    return (Stream<B>)a;
+  }
+
+  public static <B> Stream<B> point(final B b){
+    return new Cons<>(b, Lazy.value(empty()));
+  }
+
+  public static final MonadPlus<z> monadPlus = Instance.instance;
+
+  private static final class Instance implements MonadPlus.WithDefault<z>, Foldable<z> {
+    private static final Instance instance = new Instance();
+
+    private Instance(){}
+
+    @Override
+    public <A> _1<z, A> point(F0<A> a) {
+      return Stream.point(a.apply());
+    }
+
+    @Override
+    public <A, B> _1<z, B> flatMap(F1<A, _1<z, B>> f, _1<z, A> fa) {
+      final F1<A, Stream<B>> f0 = f.map(Stream::narrow);
+      return narrow(fa).flatMap(f0);
+    }
+
+    @Override
+    public <A> _1<z, A> empty() {
+      return Stream.empty();
+    }
+
+    @Override
+    public <A> _1<z, A> plus(_1<z, A> a1, _1<z, A> a2) {
+      return narrow(a1).append(() -> narrow(a2));
+    }
+
+    @Override
+    public <A, B> B foldMap(F1<A, B> f, _1<z, A> fa, Monoid<B> B) {
+      return narrow(fa).foldLeft(B.zero(), (b, a) -> B.append(b, f.apply(a)));
+    }
+
+    @Override
+    public <A, B> B foldLeft(_1<z, A> fa, B z, F2<B, A, B> f) {
+      return narrow(fa).foldLeft(z, f);
     }
   }
 }
